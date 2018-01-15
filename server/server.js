9e21825f22f1d6b80cb3658f94e76844d54bc3ac
Tiffani <tiffani.grace.elmore@gmail.com>
2018-01-14 21:48:10 -0600

    unignore

--- server/server.js
+++ server/server.js
@@ -39,9 +39,9 @@ mongoose.connection.openUri(uristring, function (err, res) {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 app.get('/api/newapp', function(req, res){
 	const hello = "good morning";
