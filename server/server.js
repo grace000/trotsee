c5b23fea8647365d8cb7c4d19000feadd5b7959a
Tiffani <tiffani.grace.elmore@gmail.com>
2018-01-14 22:00:13 -0600

    build to public

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ mongoose.connection.openUri(uristring, function (err, res) {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build'));
+  res.sendFile(path.join(__dirname+'/client/public'));
 });
 
 app.get('/api/newapp', function(req, res){
