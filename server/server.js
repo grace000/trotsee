011afc819dcc00b7453cd5e5aac7e78866597736
Tiffani <tiffani.grace.elmore@gmail.com>
2018-01-14 22:10:42 -0600

    delete router

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-router.get('/', function(req, res){
+app.get('/', function(req, res){
   res.render('index')
 });
 
